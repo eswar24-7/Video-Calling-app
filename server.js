17a784d95fa1461d0103e3018d53f4c67982906e
Meejuru Eswar <60649822+eswar24-7@users.noreply.github.com>
2023-08-22 14:45:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 //Creating a  Signalling server using socket.io, express and peer
 
-const express = require("express");
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import { ExpressPeerServer } from "peer";
+import { v4 as uuidV4 } from "uuid";
+
+const port = process.env.PORT || 3000;
 const app = express();
-const server = require("http").Server(app);
-const io = require("socket.io")(server);
-const { ExpressPeerServer } = require("peer");
+const server = http.Server(app);
+const io = new Server(server);
 const peerServer = ExpressPeerServer(server, {
   debug: true
 });
-const { v4: uuidV4 } = require("uuid");
 
 app.use("/peerjs", peerServer);
-
-// Calling our html page to open
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+// Calling our html page to open
 app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room })
 });
@@ -34,14 +38,17 @@ io.on("connection", socket => {
 
   // Connecting the user to other users
   socket.on("join-room", (roomId, userId) => {
-    socket.join(roomId)
+    socket.join(roomId);
     socket.to(roomId).emit("user-connected", userId);
     io.to(roomId).emit("user-joined", users[socket.id])
 
     // sending the message and time of message from one user to other
     socket.on("message", (message) => {
-      io.to(roomId).emit("createMessage", { message: message, userName: users[socket.id]})
-      }); 
+      io.to(roomId).emit("createMessage", { 
+        message: message, 
+        userName: users[socket.id]
+      });
+    }); 
     
     // Disconnectiong the user
     socket.on("disconnect", () => {
@@ -51,4 +58,6 @@ io.on("connection", socket => {
     });
 });
 
-server.listen(process.env.PORT||3000);
+server.listen(port, () => {
+  console.log(`server started on port ${port}.`);
+});
